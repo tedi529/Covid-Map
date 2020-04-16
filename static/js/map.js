0188f95d7ee1a0af775d9a09d0cea54b26b85530
TLatinska <teodoralatinska@hotmail.com>
2020-04-16 00:28:47 -0400

    Add legend and tooltip

--- static/js/map.js
+++ static/js/map.js
@@ -1,7 +1,7 @@
 // County geojson object thanks to: https://eric.clst.org/tech/usgeojson/
 // From data compiled by https://www.census.gov/en.html
 
-// Helper functions
+// Declare global variables and helper functions
 let day_length = 24 * 60 * 60;
 let cases = [];
 let fipsMap = {};
@@ -10,6 +10,7 @@ function makeDate(my_str) {
     return new Date(my_str);
 }
 
+
 function timestamp(str) {
     return makeDate(str).getTime();
   }
@@ -17,32 +18,70 @@ function timestamp(str) {
 function two_digit(entry) {
     my_char = entry.toString() 
         if(my_char.length === 1){
-        return '0'+my_char
+        return '0'+my_char;
     } else{
-        return my_char
+        return my_char;
     }
-}
-
-function getColor(feature, date) {
-    let datesMap = fipsMap[feature.properties.FIPS] || {};
-    let casesCount = parseInt(datesMap[date], 10) || 0;
-
-    return casesCount > 1000 ? "#662506" :
-           casesCount > 400 ? "#993404" :
-           casesCount > 80  ? "#cc4c02" :
-           casesCount > 40  ? "#ec7014" :
-           casesCount > 10  ? "#fe9929" :
-           casesCount > 5  ? "#fec44f" :
-           casesCount > 0     ? "#fee391" :
-                                "#fff7bc";
 }
 
 function formatDate(date) {
     month = two_digit(date.getMonth()+1);
-    num_date = two_digit(date.getDate()+1);
+    num_date = two_digit(date.getDate());
     return date.getFullYear() + "-" + month + "-" + num_date;
 }
 
+// Function to return object from array
+function objectify(array) {
+    var object = {};
+    array.forEach(function(element) {
+        object[element[0]] = element[1];
+    });
+    return object;
+}
+
+// Color function based on count of cases
+function getColor(feature, date) {
+    let datesMap = fipsMap[feature.properties.FIPS] || {};
+    let casesCount = parseInt(datesMap[date], 10) || 0;
+
+    return casesCount > 15000 ? "#000000" :
+           casesCount > 10000 ? "#180901" :
+           casesCount > 5000  ? "#481a04" :
+           casesCount > 1000  ? "#662506" :
+           casesCount > 500   ? "#993404" :
+           casesCount > 100   ? "#cc4c02" :
+           casesCount > 50    ? "#ec7014" :
+           casesCount > 25    ? "#fe9929" :
+           casesCount > 5     ? "#fec44f" :
+           casesCount > 0     ? "#fee391" :
+                                "#ffffff";
+}
+
+// Count function based on county and date
+function getCount(feature, date) {
+    let datesMap = fipsMap[feature.properties.FIPS] || {};
+    let casesCount = parseInt(datesMap[date], 10) || 0;
+
+    return casesCount;
+}
+
+// Function to make legend color
+function getLegendColor(d) {
+    return d > 15000 ? "#000000" :
+           d > 10000 ? "#180901" :
+           d > 5000  ? "#481a04" :
+           d > 1000  ? "#662506" :
+           d > 500   ? "#993404" :
+           d > 100   ? "#cc4c02" :
+           d > 50    ? "#ec7014" :
+           d > 25    ? "#fe9929" :
+           d > 5     ? "#fec44f" :
+           d > 0     ? "#fee391" :
+                       "#ffffff";
+
+}
+
+// Function to link counties object with cases object
 function transformCasesToFipsMap(cases) {
     cases.forEach(function (county) {
         let dateKeys = county.cases.map(el => el.date);
@@ -66,17 +105,6 @@ Object.entries(countiesObject).forEach(element => {
     properties.FIPS = fips;
 });
 
-// Function to return object from array
-function objectify(array) {
-    var object = {};
-    array.forEach(function(element) {
-        object[element[0]] = element[1];
-    });
-    return object;
-}
-
-// Initialize new cases object
-cases = [];
 
 // Read in county cases csv data
 d3.csv("static/data/counties_cases.csv").then(function(data) {
@@ -102,21 +130,18 @@ d3.csv("static/data/counties_cases.csv").then(function(data) {
             delete info[i]; 
         }
 
-        // console.log(info);
-
         let casesClean = objectify(info);
         cases.push(casesClean);
     });  
     
-    console.log(cases);
     transformCasesToFipsMap(cases);
     initMap();
 
 }); 
 
-
+// Load base map
 function initMap() {
-    // Load base map
+ 
     let mapboxAccessToken = API_KEY;
     let map = L.map('map').setView([37.8, -96], 4);
 
@@ -127,36 +152,57 @@ function initMap() {
         zoomOffset: -1
     }).addTo(map);
 
-
+    // Load first map layer on initialization
     fill_layer = L.geoJson(countiesObject, {
         style: (feature) => {
             return {
                 weight: 0.2,
-                fillColor: getColor(feature, "2020-01-21"),
+                fillColor: getColor(feature, "2020-01-20"),
                 fillOpacity: 1,
                 opacity: 1
             };
         }
-    }).addTo(map);
+    // }).bindTooltip(function (layer) {
+    //     return layer.feature.properties.NAME;
+    })
+    .addTo(map);
+
+    // Add legend
+    let legend = L.control({position: "bottomright"});
+    
+    legend.onAdd = function (map) {
+        let div = L.DomUtil.create('div', 'info legend');
+        grades = [0, 5, 25, 50, 100, 500, 1000, 5000, 10000, 15000];
+        labels = [];
+        
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getLegendColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        } 
+        
+        return div;
+    };
+
+    legend.addTo(map);
+
 }
 
+// Function to update map on new date in slider
 function updateMap(chosenDate) {        
-    function style(feature) {
-      return {
-        fillColor: getColor(feature, chosenDate),
-        opacity: 1,
-        weight: 0.1,
-        color: 'black'
-      };
-    }
 
     fill_layer.eachLayer(function (layer) {  
         layer.setStyle({fillColor : getColor(layer.feature, chosenDate)});
+        
+        layer.bindTooltip(function(layer){
+            return "County: " + layer.feature.properties.NAME + '<br>'
+                    + "Cases: " + getCount(layer.feature, chosenDate);
+        });
     });
 
 }
 
-// Create function on slider movement
+// Function on slider movement
 function updateSlider(chosenDate) {
     slider.property("value");
     d3.select(".day").text(chosenDate);
@@ -165,7 +211,8 @@ function updateSlider(chosenDate) {
 
 // Make Slider
 let minDate = new Date('2020-01-21').getTime() / 1000;
-let maxDate = new Date('2020-04-07').getTime() / 1000;
+let maxDate = new Date('2020-04-08').getTime() / 1000;
+
 
 let slider = d3.select(".slider")
     .append("input")
@@ -180,7 +227,6 @@ let slider = d3.select(".slider")
         updateSlider(chosenDate);
         updateMap(chosenDate);
     })
-
-updateSlider('2020-01-21');    
-    
+  
+updateSlider("2020-01-20");    
 
